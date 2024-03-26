f137ec9fc758e1ac10c5898ff3910d15f2d1e924
bumkey <jsbebe02@acornsoft.io>
2024-03-26 10:06:54 +0900

    랜덤 시간(환경변수)로 로깅

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import time
 from random import randint
 from flask import Flask, request
 from logging.handlers import RotatingFileHandler
@@ -7,13 +9,13 @@ import logging
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-# 로그 핸들러 설정
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler = RotatingFileHandler('/var/log/app.log', maxBytes=1024 * 1024 * 100, backupCount=20)
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-app.logger.addHandler(file_handler)
+
+# # 로그 핸들러 설정
+# file_handler = RotatingFileHandler('/var/log/app.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+# file_handler.setLevel(logging.DEBUG)
+# file_handler.setFormatter(formatter)
+# app.logger.addHandler(file_handler)
 
 # 콘솔 핸들러
 console_handler = logging.StreamHandler(sys.stdout)
@@ -21,28 +23,38 @@ console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(formatter)
 app.logger.addHandler(console_handler)
 
+
 # 예제 라우트
 @app.route('/')
 def index():
-    app.logger.debug('This is a DEBUG message')
-    app.logger.info('This is an INFO message')
-    app.logger.warning('This is a WARNING message')
-    app.logger.error('This is an ERROR message')
-    return 'Check the logs!'
+    app.logger.info('--- PYTHON APPLICATION START ---')
+    app.logger.info('This logging App for Log Service')
 
+    randomSec = randint(1, 59)
+    logging_interval_minutes = int(os.getenv('LOGGING_INTERVAL_SECOND', randomSec))
+
+    while randomSec < 60:
+        # 환경 변수에서 로깅 간격(초)을 읽기
+
+        app.logger.warn('Logging interval set to %d seconds', logging_interval_minutes)
+
+        # 주어진 간격(초)마다 로깅을 수행
+        app.logger.warn('Waiting for %d second(s) before logging again...', logging_interval_minutes)
+        time.sleep(logging_interval_minutes)
+
+        randomSec = randint(1, 30)
+        logging_interval_minutes = int(os.getenv('LOGGING_INTERVAL_SECOND', randomSec))
+        app.logger.info('---- Time is TicTok ----')
 
 
 @app.route("/rolldice")
 def roll_dice():
     player = request.args.get('player', default=None, type=str)
     result = str(roll())
-    print('시작')
-    app.logger.info('시자악합니다')
+    app.logger.info('시작')
     if player:
-        print('성공!!!!')
         app.logger.warning("%s is rolling the dice: %s", player, result)
     else:
-        print('실패!!!!')
         app.logger.warning("Anonymous player is rolling the dice: %s", result)
     return result
 
@@ -52,5 +64,4 @@ def roll():
 
 
 if __name__ == '__main__':
-    app.logger.info('min')
-    app.run()
+    app.run(debug=False)
