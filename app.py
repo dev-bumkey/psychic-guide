80c93ab4407e2c8949311bfc1369b0b04380844d
bumkey <jsbebe02@acornsoft.io>
2024-03-26 12:56:55 +0900

    수정-3

--- app.py
+++ app.py
@@ -12,17 +12,18 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# # 로그 핸들러 설정
-# file_handler = RotatingFileHandler('/var/log/app.log', maxBytes=1024 * 1024 * 100, backupCount=20)
-# file_handler.setLevel(logging.DEBUG)
-# file_handler.setFormatter(formatter)
-# app.logger.addHandler(file_handler)
-
 # 콘솔 핸들러
 console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(formatter)
 app.logger.addHandler(console_handler)
+
+
+# # 로그 핸들러 설정
+# file_handler = RotatingFileHandler('/var/log/app.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+# file_handler.setLevel(logging.DEBUG)
+# file_handler.setFormatter(formatter)
+# app.logger.addHandler(file_handler)
 
 
 # 예제 라우트
@@ -74,4 +75,8 @@ def roll():
 
 
 if __name__ == '__main__':
+    # 로그를 작성하는 스레드 시작
+    log_thread = threading.Thread(target=index)
+    log_thread.daemon = True
+    log_thread.start()
     app.run(debug=False)
