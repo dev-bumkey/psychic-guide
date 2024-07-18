20b1ba9c174db18ce38b38ead613c8f40cd6b30f
bumkey <jsbebe02@acornsoft.io>
2024-07-18 11:13:53 +0900

    fastapi 적용

--- app.py
+++ app.py
@@ -2,37 +2,41 @@ import sys
 import os
 import threading
 import time
+import logging
 from logging.handlers import RotatingFileHandler
 from random import randint
-from flask import Flask, request, redirect
-from flask import g
-import logging
+from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
+import uvicorn
 
-app = Flask(__name__)
+app = FastAPI()
 logger = logging.getLogger(__name__)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter1 = logging.Formatter('Stream :  %(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # 콘솔 핸들러
 console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
-app.logger.addHandler(console_handler)
 logger.setLevel(logging.DEBUG)
-# # 로그 핸들러 설정
-# file_handler = RotatingFileHandler('/var/log/app.log', maxBytes=1024 * 1024 * 100, backupCount=20)
-# file_handler.setLevel(logging.DEBUG)
-# file_handler.setFormatter(formatter)
-# logger.addHandler(file_handler)
+
+
+# 스트림 핸들러
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+stream_handler.setFormatter(formatter1)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
 
 
 
 # 백그라운드 스레드와 루프를 컨트롤할 플래그
 keep_running = True
 loop_thread = None
-# 예제 라우트
-@app.route('/')
-def index():
+
+@app.get("/")
+async def index():
     global loop_thread
     if loop_thread is None or not loop_thread.is_alive():
         # 루프 스레드가 없거나 종료되었을 때에만 새로운 스레드 시작
@@ -40,8 +44,7 @@ def index():
         loop_thread.start()
     logger.info('--- PYTHON APPLICATION START ---')
     logger.debug('This logging App for Log Service')
-    return "Loop started"
-
+    return PlainTextResponse("Loop started")
 
 def loop_function():
     global keep_running
@@ -56,24 +59,21 @@ def loop_function():
         logging_interval_minutes = int(os.getenv('LOGGING_INTERVAL_SECOND', randomSec))
         logger.info('---- Time is TicTok ----')
 
-
-@app.route("/rolldice")
-def roll_dice():
-    player = request.args.get('player', default=None, type=str)
+@app.get("/rolldice")
+async def roll_dice(request: Request):
+    player = request.query_params.get('player')
     result = str(roll())
     logger.info('시작')
     if player:
         logger.warning("%s is rolling the dice: %s", player, result)
     else:
         logger.warning("Anonymous player is rolling the dice: %s", result)
-    return result
-
+    return PlainTextResponse(result)
 
 def roll():
     return randint(1, 6)
 
-
 if __name__ == '__main__':
     # log = logging.getLogger('werkzeug')
     # log.setLevel(logging.DEBUG)
-    app.run(debug=False)
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
